d0ee01d1604ac3d9bafd4a1e6a2b556a0c37f54e
Vinicius Petris <vinicius.petris@b2wdigital.com>
2022-03-13 23:23:03 -0300

    add ex 1

---
+++ Atividade_1/Motor.java
@@ -0,0 +1,33 @@
+package Atividade_1;
+
+public class Motor {
+    private int qtdPist;
+
+    private int potencia;
+
+    public Motor() {
+        this.qtdPist = 1;
+        this.potencia = 1;
+    }
+
+    public Motor(int qtdPist, int potencia) {
+        this.qtdPist = qtdPist;
+        this.potencia = potencia;
+    }
+
+    public int getQtdPist() {
+        return qtdPist;
+    }
+
+    public void setQtdPist(int qtdPist) {
+        this.qtdPist = qtdPist;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+}
